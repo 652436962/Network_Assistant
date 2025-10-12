9a9c0048428bd855a37310b65cb5c0c8a3aeef86
cai-yunwei <1261820315@qq.com>
2025-10-12 23:43:22 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿// CMakeProjectTest.cpp: 定义应用程序的入口点。
+//
+
+#include <iostream>
+#include <QApplication>
+#include "main_window.h"
+
+int main(int argc, char* argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
